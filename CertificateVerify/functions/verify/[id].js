e776256b55af32da77ea8691d39f9207bcc94723
madhan-890 <madhanthota60@gmail.com>
2026-03-03 16:26:35 +0530

    Updated

--- CertificateVerify/functions/verify/[id].js
+++ CertificateVerify/functions/verify/[id].js
@@ -25,18 +25,28 @@ export async function onRequest(context) {
   const res = await context.next();
   let html = await res.text();
 
+  const verifyUrl = `https://yourdomain.com/verify/${id}`;
+
   html = html.replace(
     "</head>",
     `
     <meta property="og:title" content="${cert.name} - ${cert.course}" />
-    <meta property="og:description" content="Issued by ${cert.issuer} on ${cert.issue_date}" />
+    <meta property="og:description" content="Certificate issued by ${cert.issuer} on ${cert.issue_date}" />
     <meta property="og:image" content="${cert.certificate_url}" />
+    <meta property="og:url" content="${verifyUrl}" />
     <meta property="og:type" content="website" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="${cert.name} - ${cert.course}" />
+    <meta name="twitter:description" content="Certificate issued by ${cert.issuer}" />
+    <meta name="twitter:image" content="${cert.certificate_url}" />
     </head>
     `
   );
 
   return new Response(html, {
-    headers: { "content-type": "text/html" },
+    headers: { "content-type": "text/html; charset=UTF-8" },
   });
 }
